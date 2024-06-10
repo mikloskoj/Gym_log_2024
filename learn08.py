57bb2dcb8845a89fa01777560a26b3cd3a0fa7c9
Jan Mikloško <jan.miklosko@outlook.com>
2024-06-10 21:26:07 +0200

    New chunk of code.

--- learn08.py
+++ learn08.py
@@ -1,84 +1,101 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 import seaborn as sns
 
 file_path = r'C:\Users\janmi\Documents\VS Code\Gym_log_2024\gym_log_Q1_2024 - workout data.csv'
 body_weight = 79
+
+# outliers = ['Plank']
 selected_exercises = ('Kneeling dip', 'Bench press', 'Chest press', 'Prone leg curl', 'Lat pulldown', 'Bicep curl')
-
-
-
 
 def filter_remove_duplicities(df) -> None:
     filtered_df = df[df['Muscle group'] != "Cardio"]
     filtered_df = filtered_df['Muscle group'].drop_duplicates()
     print(filtered_df)
 
-def total_volumes_kneeling_dip(ax, df, body_weight) -> None:
+def total_volumes(ax1, ax2, df, body_weight, selected_exercises, window=8) -> None:
+    # Convert 'Date' column to datetime
     df['Date'] = pd.to_datetime(df['Date'], errors='coerce', dayfirst=True)
-    filtered_df = df[(df['Muscle group'] != 'Cardio') & (df['Muscle group'] != 'Walk')].copy()
-    filtered_df = filtered_df[filtered_df['Exercise name'] == 'Kneeling dip']
-
-    filtered_df['Weight'] = filtered_df['Weight'].str.replace(',', '.')
-    filtered_df['Weight'] = pd.to_numeric(filtered_df['Weight'], errors='coerce')
-    filtered_df['Reps'] = pd.to_numeric(filtered_df['Reps'], errors='coerce')
-    filtered_df['Sets'] = pd.to_numeric(filtered_df['Sets'], errors='coerce')
-
-    filtered_df.loc[filtered_df['Body weight flg'] == 'BW', 'Weight'] += body_weight
-    filtered_df['Reps_times_Sets'] = filtered_df['Reps'] * filtered_df['Sets']
-    filtered_df['total_weight'] = filtered_df['Reps'] * filtered_df['Sets'] * filtered_df['Weight']
-
-    grouped_df = filtered_df.groupby('Date')[['Sets', 'Reps', 'Weight', 'Reps_times_Sets', 'total_weight']].sum()
     
-    normalized_weights = (grouped_df['total_weight'] - grouped_df['total_weight'].min()) / (grouped_df['total_weight'].max() - grouped_df['total_weight'].min())
-    cmap = plt.get_cmap('YlOrRd')
-    colors = cmap(normalized_weights)
-    
-    ax.bar(grouped_df.index, grouped_df['total_weight'], color=colors)
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Total Weight')
-    ax.set_title('Total Weight Lifted Over Time - Kneeling dip', fontweight='bold')
-    ax.tick_params(axis='x', rotation=45)
-
-
-def total_volumes(ax, df, body_weight, selected_exercises) -> None:
-    df['Date'] = pd.to_datetime(df['Date'], errors='coerce', dayfirst=True)
+    # Filter the dataframe based on muscle group and selected exercises
+    # filtered_df = df[(df['Muscle group'] != 'Cardio') & (df['Muscle group'] != 'Walk')].copy()
     filtered_df = df[(df['Muscle group'] != 'Cardio') & (df['Muscle group'] != 'Walk') & (df['Exercise name'].isin(selected_exercises))].copy()
-
+    
+    # Replace commas in 'Weight' column and convert to numeric
     filtered_df['Weight'] = filtered_df['Weight'].str.replace(',', '.')
     filtered_df['Weight'] = pd.to_numeric(filtered_df['Weight'], errors='coerce')
     filtered_df['Reps'] = pd.to_numeric(filtered_df['Reps'], errors='coerce')
     filtered_df['Sets'] = pd.to_numeric(filtered_df['Sets'], errors='coerce')
-
-    filtered_df.loc[filtered_df['Body weight flg'] == 'BW', 'Weight'] += body_weight
-    filtered_df['Reps_times_Sets'] = filtered_df['Reps'] * filtered_df['Sets']
-    filtered_df['total_weight'] = filtered_df['Reps'] * filtered_df['Sets'] * filtered_df['Weight']
-
-    grouped_df = filtered_df.groupby(['Exercise name'])[['Sets', 'Reps', 'Weight', 'Reps_times_Sets', 'total_weight']].sum()
-    grouped_df = grouped_df.sort_values('total_weight', ascending=False)
     
-    sns.barplot(data=grouped_df.reset_index(), x='Exercise name', y='total_weight',hue='total_weight' , ax=ax, palette='YlOrRd')
-    ax.set_xlabel('Exercise')
-    ax.set_ylabel('Total Weight')
-    ax.set_title('Total Weight Lifted Over Time', fontweight='bold')
+    # Adjust weights for body weight exercises
+    filtered_df.loc[filtered_df['Body weight flg'] == 'BW', 'Weight'] += body_weight
+    
+    # Calculate total reps and total weight
+    filtered_df['total_reps'] = filtered_df['Reps'] * filtered_df['Sets']
+    filtered_df['total_weight'] = filtered_df['Reps'] * filtered_df['Sets'] * filtered_df['Weight']
+    
+    # Group by 'Exercise name' and sum the relevant columns for total summary
+    grouped_df_total = filtered_df.groupby(['Exercise name'])[['Sets', 'Reps', 'Weight', 'total_reps', 'total_weight']].sum()
+    grouped_df_total = grouped_df_total.sort_values('total_reps', ascending=False)
 
-    table_data = grouped_df.reset_index()
-    table = ax.table(cellText=table_data.values, colLabels=table_data.columns, cellLoc='center', loc='bottom', bbox=[0, -0.5, 1, 0.34])
+    # Keep only the top 3 exercises
+    top_exercises = grouped_df_total.head(5)
+
+    # Filter the main dataframe to keep only the top 3 exercises
+    filtered_df = filtered_df[filtered_df['Exercise name'].isin(top_exercises.index)]
+
+    # Update grouped_df with only the top 3 exercises
+    grouped_df = filtered_df.groupby(['Date', 'Exercise name'])[['Sets', 'Reps', 'Weight', 'total_reps', 'total_weight']].sum().reset_index()
+
+    # Apply moving average
+    grouped_df['total_reps_ma'] = grouped_df.groupby('Exercise name')['total_reps'].transform(lambda x: x.rolling(window=window, min_periods=1).mean())
+
+    # Create a palette and use it for both plots based on sorted total reps
+    unique_exercises = top_exercises.index
+    palette = sns.color_palette('YlOrRd_r', len(unique_exercises))
+    color_mapping = dict(zip(unique_exercises, palette))
+
+    # Plot line plot with moving average
+    sns.lineplot(
+        ax=ax1,
+        data=grouped_df,
+        x='Date',
+        y='total_reps_ma',
+        hue='Exercise name',
+        palette=color_mapping, 
+        marker='v'
+    )
+
+    ax1.tick_params(axis='x', rotation=45)
+
+    # Plot bar plot
+    sns.barplot(
+        data=top_exercises.reset_index(),
+        x='Exercise name',
+        y='total_reps',
+        ax=ax2,
+        palette=color_mapping
+    )
+
+    ax2.set_xlabel('Exercise')
+    ax2.set_ylabel('Total Reps')
+    ax2.set_title('Total Reps Lifted Over Time', fontweight='bold')
+    
+    # Add table
+    table_data = top_exercises.reset_index()
+    table = ax2.table(cellText=table_data.values, colLabels=table_data.columns, cellLoc='center', loc='bottom', bbox=[0, -0.5, 1, 0.34])
     table.auto_set_font_size(False)
     table.set_fontsize(8)
     table.scale(1, 0.5)
-
+    
     for key, cell in table.get_celld().items():
         if key[0] == 0:
             cell.set_text_props(weight='bold', color='white', fontsize=6)
             cell.set_facecolor('#40466e')
         else:
             cell.set_facecolor('#f2f2f2')
-
-
+    
     plt.subplots_adjust(left=0.1, bottom=0.2)
-
 
 def main() -> None:
     try:
@@ -87,20 +104,13 @@ def main() -> None:
         print(f"File not found. Details: {e}")
         return
 
-
     # Gym Workout Analysis 2024 - Total volumes
     fig, (ax2, ax1) = plt.subplots(1, 2, figsize=(14, 7))
-    total_volumes_kneeling_dip(ax1, df, body_weight)
-    total_volumes(ax2, df, body_weight, selected_exercises)
-    fig.text(0.95, 0.1, 'Gym Workout Analysis Q1 2024\nThis shows the total volumes I have lifted since 1st of January.\nI hope you like my charts and everything is clear.', ha='right', va='center', fontsize=10, fontweight='normal')
+    total_volumes(ax1, ax2, df, body_weight, selected_exercises)
+    fig.text(0.95, 0.1, f'Gym Workout Analysis Q1 2024\nThis shows the total volumes I have lifted since 1st of January.\nI hope you like my charts and everything is clear.\nselected_exercises: {selected_exercises} were excluded.', ha='right', va='center', fontsize=10, fontweight='normal')
     fig.canvas.manager.set_window_title('Gym Workout Analysis 2024 - Total volumes')
     plt.tight_layout()
     plt.show()
-
-
-    # Different
-
-
 
 if __name__ == "__main__":
     main()
--- learn09.py
+++ learn09.py
@@ -1,14 +1,82 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+# File path and initial parameters
 file_path = r'C:\Users\janmi\Documents\VS Code\Gym_log_2024\gym_log_Q1_2024 - workout data.csv'
 body_weight = 79
 
-df = pd.read_csv(file_path, sep=';', encoding='latin1')
 
-df['Weight'] = df['Weight'].str.replace(',', '.')
-df['Weight'] = pd.to_numeric(df['Weight'], errors='coerce')
-df['Date'] = pd.to_datetime(df['Date'])
+def daily_view(df, window=8) -> None:
+    df_weekly = df.groupby(['Week'])[['Sets']].sum()
+    df_monthly = df.groupby(['Month'])[['Sets']].sum()
+    df_daily = df.groupby(['Date'])[['Sets']].sum()
+    df_daily = df_daily.groupby('Date')['Sets'].transform(lambda x: x.rolling(window=window, min_periods=1).mean())
 
-df = df[['Date', 'Weight']]
 
-print(df.head())
+    fig, ax = plt.subplots(1, 3, figsize=(14, 6))  # Create a figure and two subplots
+
+    sns.barplot(
+        data=df_monthly.reset_index(),
+        x='Month',
+        y='Sets',
+        hue='Sets',
+        palette='YlOrRd',
+        ax=ax[0]
+    )
+    ax[0].set_title('Monthly Sets')
+
+
+    sns.barplot(
+        data=df_weekly.reset_index(),
+        x='Week',
+        y='Sets',
+        hue='Sets',
+        palette='YlOrRd',
+        ax=ax[1]
+    )
+    ax[1].set_title('Weekly Sets')
+
+
+
+    sns.lineplot(
+        data=df_daily.reset_index(),
+        x='Date',
+        y='Sets',
+        ax=ax[2]
+    )
+    ax[2].set_title('Daily Sets')
+    ax[2].set_xticklabels([])
+
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    plt.show()
+
+
+def main() -> None:
+    try:
+        df = pd.read_csv(file_path, sep=';', index_col=None, encoding='latin1')
+        
+        # Data cleaning
+        df['Date'] = pd.to_datetime(df['Date'], errors='coerce', dayfirst=True)
+        df['Weight'] = df['Weight'].str.replace(',', '.')
+        df['Weight'] = pd.to_numeric(df['Weight'], errors='coerce')
+        df['Reps'] = pd.to_numeric(df['Reps'], errors='coerce')
+        df['Sets'] = pd.to_numeric(df['Sets'], errors='coerce')
+
+        # Add week number column
+        df['Week'] = df['Date'].dt.isocalendar().week
+        df['Month'] = df['Date'].dt.month
+
+        df = df.sort_values(by='Date', ascending=True)
+
+
+    except FileNotFoundError as e:
+        print(f"File not found. Details: {e}")
+        return
+
+    daily_view(df)
+
+
+if __name__ == "__main__":
+    main()
