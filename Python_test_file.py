80d026fb330e3febbcaff7d3cb6a2a9997ef2794
Jan Mikloško <jan.miklosko@outlook.com>
2024-06-17 13:49:41 +0200

    Added exercise_volumes function

--- Python_test_file.py
+++ Python_test_file.py
@@ -1,98 +1,130 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.signal import savgol_filter
+import seaborn as sns
 
-
-df = pd.read_excel(r'C:\Users\janmi\Documents\VS Code\Gym_log_2024\gym_log_Q1_2024 - workout data.xlsx')
-df = df.sort_values('Date', ascending=True)
-
-
+file_path = 'gym_log_Q1_2024 - workout data.csv'
 body_weight = 79
+sns.set_style("white")
+border_color = 'lightgrey'
+background_color = '#fdfcfc'
 
+# outliers = ['Plank']
+selected_exercises = ('Kneeling dip', 'Bench press', 'Chest press', 'Prone leg curl', 'Lat pulldown', 'Bicep curl')
 
-# If the weight is negative then calculate body weight - weight
-# Negative weight is when the exercise is supported.
-df['Weight'] = df['Weight'].apply(lambda x: body_weight + x if x < 0 else x)
+def filter_remove_duplicities(df) -> None:
+    filtered_df = df[df['Muscle group'] != "Cardio"]
+    filtered_df = filtered_df['Muscle group'].drop_duplicates()
+    print(filtered_df)
 
-# If the exercise is with body weight then calculate body weight + weight
-df.loc[df['Body weight flg'] == 'BW', 'Weight'] = df.loc[df['Body weight flg'] == 'BW', 'Weight'].apply(lambda x: body_weight + x)
-
-# Exercises to compare
-exercises_to_compare = ["Kneeling dip", "Squat", "Bench press"]
-
-
-def plot_weight_over_time(df, exercises):
-    """Plot the weight over time for selected exercises using smoothed line charts and display a summary table."""
+def total_volumes(ax1, ax2, df, body_weight, selected_exercises, window=8) -> None:
+    # Convert 'Date' column to datetime
+    df['Date'] = pd.to_datetime(df['Date'], errors='coerce', dayfirst=True)
     
-    fig, (plt1, plt2, plt3) = plt.subplots(3, 1, figsize=(12, 12))
-
-    # Define colors for specific exercises
-    color_map = {
-        'Kneeling dip': '#2f6624',
-        'Squat': '#f2ba02',  # You can add more exercises and their colors here
-        # Add other exercises and their colors if needed
-    }
-
-    for exercise_name in exercises:
-        filtered_df = df[df['Exercise name'] == exercise_name]
-        
-        # Group by Date and find the max weight for each date
-        max_weights = filtered_df.groupby('Date')['Weight'].max().reset_index()
-
-        # Apply Savitzky-Golay filter to smooth the lines
-        smoothed_weights = savgol_filter(max_weights['Weight'], window_length=5, polyorder=2)
-
-        # Get color for the exercise, default to None if not in the map
-        color = color_map.get(exercise_name, None)
-        
-        # Plot the smoothed line chart on the first subplot
-        plt1.plot(max_weights['Date'], smoothed_weights, label=exercise_name, linewidth=3, alpha=0.7, color=color)
-
-    plt1.set_title('Weight Over Time for Selected Exercises')
-    plt1.set_xlabel('Date')
-    plt1.set_ylabel('Weight (Kg)')
-    plt1.legend()
-    plt1.grid(True)
-
-    # Placeholder for the second subplot (you can customize it as needed)
-    plt2.set_title('Placeholder for Second Plot')
-    plt2.grid(True)
-
-    # Create summary table data
-    summary_data = {
-        'Exercise': [],
-        'Max Weight': [],
-        'Min Weight': []
-    }
-
-    for exercise_name in exercises:
-        filtered_df = df[df['Exercise name'] == exercise_name]
-        max_weight = filtered_df['Weight'].max()
-        min_weight = filtered_df['Weight'].min()
-        summary_data['Exercise'].append(exercise_name)
-        summary_data['Max Weight'].append(max_weight)
-        summary_data['Min Weight'].append(min_weight)
-
-    summary_df = pd.DataFrame(summary_data)
-
-    # Plot the summary table on the third subplot
-    plt3.axis('tight')
-    plt3.axis('off')
-    table = plt3.table(cellText=summary_df.values, colLabels=summary_df.columns, cellLoc='center', loc='center')
-
-    # Make the header bold
-    for (i, j), cell in table.get_celld().items():
-        if i == 0:  # Header cells
-            cell.set_text_props(weight='bold', color='Grey', fontsize=7)
-        
-        cell.set_edgecolor('black')
-        cell.set_linewidth(0.5)  # Set the line width of the cell borders
-
-    plt3.set_title('Summary Table of Max and Min Weights for Each Exercise')
+    # Filter the dataframe based on muscle group and selected exercises
+    filtered_df = df[(df['Muscle group'] != 'Cardio') & (df['Muscle group'] != 'Walk') & (df['Exercise name'].isin(selected_exercises))].copy()
     
-    fig.subplots_adjust(hspace=0.5)
+    # Replace commas in 'Weight' column and convert to numeric
+    filtered_df['Weight'] = filtered_df['Weight'].str.replace(',', '.')
+    filtered_df['Weight'] = pd.to_numeric(filtered_df['Weight'], errors='coerce')
+    filtered_df['Reps'] = pd.to_numeric(filtered_df['Reps'], errors='coerce')
+    filtered_df['Sets'] = pd.to_numeric(filtered_df['Sets'], errors='coerce')
+    
+    # Adjust weights for body weight exercises
+    filtered_df.loc[filtered_df['Body weight flg'] == 'BW', 'Weight'] += body_weight
+    
+    # Calculate total reps and total weight
+    filtered_df['total_reps'] = filtered_df['Reps'] * filtered_df['Sets']
+    filtered_df['total_weight'] = filtered_df['Reps'] * filtered_df['Sets'] * filtered_df['Weight']
+    
+    # Group by 'Exercise name' and sum the relevant columns for total summary
+    grouped_df_total = filtered_df.groupby(['Exercise name'])[['Sets', 'Reps', 'Weight', 'total_reps', 'total_weight']].sum()
+    grouped_df_total = grouped_df_total.sort_values('total_reps', ascending=False)
+
+    # Keep only the top 5 exercises
+    top_exercises = grouped_df_total.head(5)
+
+    # Filter the main dataframe to keep only the top 5 exercises
+    filtered_df = filtered_df[filtered_df['Exercise name'].isin(top_exercises.index)]
+
+    # Update grouped_df with only the top 5 exercises
+    grouped_df = filtered_df.groupby(['Date', 'Exercise name'])[['Sets', 'Reps', 'Weight', 'total_reps', 'total_weight']].sum().reset_index()
+
+    # Apply moving average
+    grouped_df['total_reps_ma'] = grouped_df.groupby('Exercise name')['total_reps'].transform(lambda x: x.rolling(window=window, min_periods=1).mean())
+
+    # Create a palette and use it for both plots based on sorted total reps
+    unique_exercises = top_exercises.index
+    palette = sns.color_palette('YlOrRd_r', len(unique_exercises))
+    color_mapping = dict(zip(unique_exercises, palette))
+
+    # Plot line plot with moving average
+    sns.lineplot(
+        ax=ax1,
+        data=grouped_df,
+        x='Date',
+        y='total_reps_ma',
+        hue='Exercise name',
+        palette=color_mapping, 
+        marker='v'
+    )
+
+    ax1.tick_params(axis='x', rotation=45)
+    ax1.set_xlabel('')
+    ax1.set_ylabel('Total Reps')
+    ax1.set_title('Total Reps Over Time', fontweight='bold')
+
+    # Plot bar plot
+    sns.barplot(
+        data=top_exercises.reset_index(),
+        x='Exercise name',
+        y='total_reps',
+        ax=ax2,
+        palette=color_mapping
+    )
+
+    ax2.set_xlabel('')
+    ax2.set_ylabel('Total Reps')
+    ax2.set_title('Total Reps Lifted Over Time', fontweight='bold')
+    
+    # Add table
+    table_data = top_exercises.reset_index()
+    table = ax2.table(cellText=table_data.values, colLabels=table_data.columns, cellLoc='center', loc='bottom', bbox=[0, -0.5, 1, 0.34])
+    table.auto_set_font_size(False)
+    table.set_fontsize(8)
+    table.scale(1, 0.5)
+
+    # Set table borders
+    for key, cell in table.get_celld().items():
+        cell.set_edgecolor(border_color)
+        cell.set_linewidth(0.5)
+        if key[0] == 0:
+            cell.set_text_props(weight='bold', color='white', fontsize=6)
+            cell.set_facecolor('#40466e')
+        else:
+            cell.set_facecolor('#f2f2f2')
+    
+    plt.subplots_adjust(left=0.1, bottom=0.2)
+
+    # Customize the plot backgrounds and spines
+    for ax in [ax1, ax2]:
+        ax.set_facecolor(background_color)
+        for spine in ax.spines.values():
+            spine.set_edgecolor(border_color)
+
+def main() -> None:
+    try:
+        df = pd.read_csv(file_path, sep=';', index_col=None, encoding='latin1')
+    except FileNotFoundError as e:
+        print(f"File not found. Details: {e}")
+        return
+
+    # Gym Workout Analysis 2024 - Total volumes
+    fig, (ax2, ax1) = plt.subplots(1, 2, figsize=(14, 7))
+    total_volumes(ax1, ax2, df, body_weight, selected_exercises)
+    fig.text(0.95, 0.1, f'Gym Workout Analysis Q1 2024\nThis shows the total volumes I have lifted since 1st of January.\nI hope you like my charts and everything is clear.\nselected_exercises: {selected_exercises} were excluded.', ha='right', va='center', fontsize=10, fontweight='normal')
+    fig.canvas.manager.set_window_title('Gym Workout Analysis 2024 - Total volumes')
+    plt.tight_layout()
     plt.show()
 
-
-# Call the function to display the plots
-plot_weight_over_time(df, exercises_to_compare)
+if __name__ == "__main__":
+    main()
--- gym_log_2024.py
+++ gym_log_2024.py
@@ -11,8 +11,9 @@ height = 181
 sns.set_style("white")
 border_color = 'lightgrey'
 background_color = '#fdfcfc'
-color_palette = sns.dark_palette("#69d", reverse=True, as_cmap=True)
-line_color = '#173b6a'
+color = "#193f71" # color
+color_palette = sns.dark_palette(color, reverse=True, as_cmap=True)
+line_color = color
 selected_exercises = ('Kneeling dip', 'Bench press', 'Chest press', 'Prone leg curl', 'Lat pulldown', 'Bicep curl')
 
 
@@ -33,7 +34,7 @@ def load_data(file_path_1, file_path_2):
 
 def data_preparation(df1, df2):
     df1['Date'] = pd.to_datetime(df1['Date'], errors='coerce', dayfirst=True)
-    df1['Weight'] = df1['Weight'].str.replace(',', '.')
+    df1['Weight'] = df1['Weight'].astype(str).str.replace(',', '.')
     df1['Weight'] = pd.to_numeric(df1['Weight'], errors='coerce')
     df1['Reps'] = pd.to_numeric(df1['Reps'], errors='coerce')
     df1['Sets'] = pd.to_numeric(df1['Sets'], errors='coerce')
@@ -225,17 +226,97 @@ def sets_view(df1, window=8) -> None:
     plt.tight_layout()
     plt.show()
 
+
+def excercise_volumes(df1, body_weight, selected_exercises, window=8):
+    filtered_df = df1[(df1['Muscle group'] != 'Cardio') & (df1['Muscle group'] != 'Walk') & (df1['Exercise name'].isin(selected_exercises))].copy()
+
+    
+    filtered_df.loc[filtered_df['Body weight flg'] == 'BW', 'Weight'] += body_weight
+    
+    filtered_df['total_reps'] = filtered_df['Reps'] * filtered_df['Sets']
+    filtered_df['total_weight'] = filtered_df['Reps'] * filtered_df['Sets'] * filtered_df['Weight']
+    
+    grouped_df_total = filtered_df.groupby(['Exercise name'])[['Sets', 'Reps', 'Weight', 'total_reps', 'total_weight']].sum()
+    grouped_df_total = grouped_df_total.sort_values('total_reps', ascending=False)
+
+    top_exercises = grouped_df_total.head(5)
+
+    filtered_df = filtered_df[filtered_df['Exercise name'].isin(top_exercises.index)]
+
+    grouped_df = filtered_df.groupby(['Date', 'Exercise name'])[['Sets', 'Reps', 'Weight', 'total_reps', 'total_weight']].sum().reset_index()
+    grouped_df['total_reps_ma'] = grouped_df.groupby('Exercise name')['total_reps'].transform(lambda x: x.rolling(window=window, min_periods=1).mean())
+
+    unique_exercises = top_exercises.index
+    palette = sns.dark_palette(color, len(unique_exercises))
+    color_mapping = dict(zip(unique_exercises, palette))
+
+    fig, (ax2, ax1) = plt.subplots(1, 2, figsize=(14, 7))
+
+    sns.lineplot(
+        ax=ax1,
+        data=grouped_df,
+        x='Date',
+        y='total_reps_ma',
+        hue='Exercise name',
+        palette=color_mapping, 
+        marker='v'
+    )
+
+    ax1.tick_params(axis='x', rotation=45)
+    ax1.set_xlabel('')
+    ax1.set_ylabel('Total Reps')
+    ax1.set_title('Total Reps Over Time', fontweight='bold')
+
+    sns.barplot(
+        data=top_exercises.reset_index(),
+        x='Exercise name',
+        y='total_reps',
+        ax=ax2, hue='Exercise name',
+        palette=color_mapping
+    )
+
+    ax2.set_xlabel('')
+    ax2.set_ylabel('Total Reps')
+    ax2.set_title('Total Reps Lifted Over Time', fontweight='bold')
+
+    table_data = top_exercises.reset_index()
+    table = ax2.table(cellText=table_data.values, colLabels=table_data.columns, cellLoc='center', loc='bottom', bbox=[0, -0.5, 1, 0.34])
+    table.auto_set_font_size(False)
+    table.set_fontsize(8)
+    table.scale(1, 0.5)
+
+    for key, cell in table.get_celld().items():
+        cell.set_edgecolor(border_color)
+        cell.set_linewidth(0.5)
+        if key[0] == 0:
+            cell.set_text_props(weight='bold', color='white', fontsize=6)
+            cell.set_facecolor('#40466e')
+        else:
+            cell.set_facecolor('#f2f2f2')
+
+    plt.subplots_adjust(left=0.1, bottom=0.2)
+
+    for ax in [ax1, ax2]:
+        ax.set_facecolor(background_color)
+        for spine in ax.spines.values():
+            spine.set_edgecolor(border_color)
+
+    fig.text(0.95, 0.1, f'Gym Workout Analysis Q1 2024\nThis shows the total volumes I have lifted since 1st of January.\nI hope you like my charts and everything is clear.\nselected_exercises: {selected_exercises} were excluded.', ha='right', va='center', fontsize=10, fontweight='normal')
+    fig.canvas.manager.set_window_title('Gym Workout Analysis 2024 - Total volumes')
+    plt.tight_layout()
+    plt.show()
+
+
 def main():
     df1, df2 = load_data(file_path_1, file_path_2)
     if df1 is not None and df2 is not None:
         df1, df2 = data_preparation(df1, df2)
-        
-        
-        body_values(df2)
 
-       
 
+        
+        excercise_volumes(df1, body_weight, selected_exercises)
         '''
+                body_values(df2)
         fig, axes = plt.subplots(1, 2, figsize=(15, 6))
         
         correlation_waist_v_weight(df2, axes[0])
@@ -243,10 +324,11 @@ def main():
         plt.show()
 
         sets_view(df1)
+
        
-        
-        
+
         '''
+
 
 if __name__ == "__main__":
     main()
